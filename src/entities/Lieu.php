76a91881f68914bc25f2cd1d05dcb1059fe8f89e
Moussa SARR <einsarr@gmail.com>
2020-01-05 12:08:32 +0000

    Implémenation de l'entité Lieu

---
+++ src/entities/Lieu.php
@@ -0,0 +1,64 @@
+<?php
+class formations
+{
+    private $id;
+    private $nom;
+    private $longitude;
+    private $latitude;
+    private $formations;
+    private $user;
+
+    public function __construct()
+    {
+        
+    }
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+    public function getFormations()
+    {
+        return $this->formations;
+    }
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+    }
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+    }
+    public function setFormations($formations)
+    {
+        $this->formations = $formations;
+    }
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+    public function toString()
+    {
+        return "Id : ".$this->id." - Nom : ".$this->nom." - Longitude : ".$this->latitude;
+    }
+}
